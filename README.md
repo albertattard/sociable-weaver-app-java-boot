8350b8f5cedfddd652ae6a02bb6021819a157ba9
Albert Attard <albert.attard@thoughtworks.com>
2021-11-24 14:30:15 +0100

    Create project

---
+++ README.md
@@ -0,0 +1,21 @@
+# Sociable Weaver
+
+## Useful commands
+
+- Run all tests
+
+  ```shell
+  $ ./gradlew check
+  ```
+
+- Run the application locally, ideal for development
+
+  ```shell
+  $ ./gradlew bootRun
+  ```
+
+- Build the application for production
+
+  ```shell
+  $ ./gradlew build
+  ```
---
+++ src/main/java/aa/sw/SwApp.java
@@ -0,0 +1,13 @@
+package aa.sw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwApp {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(SwApp.class, args);
+	}
+
+}
