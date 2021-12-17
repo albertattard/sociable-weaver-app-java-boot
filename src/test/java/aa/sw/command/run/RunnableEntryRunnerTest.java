26af08dbf898bdb93e5d1f4925947d920cb6c68f
Albert Attard <albert.attard@thoughtworks.com>
2021-12-17 17:26:06 +0100

    Improve test coverage (sociable-weaver/backlog#17)

--- src/test/java/aa/sw/command/run/RunnableEntryRunnerTest.java
+++ src/test/java/aa/sw/command/run/RunnableEntryRunnerTest.java
@@ -5,8 +5,13 @@ import aa.sw.command.RunnableEntry;
 import org.junit.jupiter.api.Test;
 
 import java.io.StringWriter;
+import java.util.Collections;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class RunnableEntryRunnerTest {
 
@@ -23,5 +28,36 @@ class RunnableEntryRunnerTest {
         /* Then */
         assertThat(result).isEqualTo(CommandResult.dryRun());
         assertThat(writer.toString()).isEqualTo("Cannot run an entry that is flagged as dry run!!");
+    }
+
+    @Test
+    void returnExecutionStrategyNotFoundIfNoStrategyIsAvailableForEntry() {
+        /* Given */
+        final RunnableEntry entry = RunnableEntry.builder().type("something").build();
+        final RunnableEntryRunner runner = new RunnableEntryRunner(Collections.emptyMap()); /* No strategies */
+        final StringWriter writer = new StringWriter();
+
+        /* When */
+        final CommandResult result = runner.run(entry, writer::write);
+
+        /* Then */
+        assertThat(result).isEqualTo(CommandResult.executionStrategyNotFound());
+        assertThat(writer.toString()).isEqualTo("No execution strategy found that can execute an entry of type something");
+    }
+
+    @Test
+    void returnExecutionStrategyResultForTheMatchingEntry() {
+        /* Given */
+        final RunnableEntry entry = RunnableEntry.builder().type("something").build();
+        final RunnableEntryExecutionStrategy strategy = mock(RunnableEntryExecutionStrategy.class);
+        final RunnableEntryRunner runner = new RunnableEntryRunner(Map.of("something", (e) -> strategy));
+        final StringWriter writer = new StringWriter();
+        when(strategy.execute(any())).thenReturn(CommandResult.finishedAsExpected());
+
+        /* When */
+        final CommandResult result = runner.run(entry, writer::write);
+
+        /* Then */
+        assertThat(result).isEqualTo(CommandResult.finishedAsExpected());
     }
 }
